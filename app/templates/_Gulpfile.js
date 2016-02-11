29c640d960117003b7159dcc82f687581fa1f6fe
Benjamin HUBERT <benjamin@alpixel.fr>
2016-02-11 18:35:19 +0100

    Working on the gulpfile generation + switch back to less

--- app/templates/_Gulpfile.js
+++ app/templates/_Gulpfile.js
@@ -1,43 +1,57 @@
-
 var gulp = require('gulp');
-
-var sass = require('gulp-ruby-sass');
+var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCss = require('gulp-minify-css');
 var watch = require('gulp-watch');
-
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
-
 var imagemin = require('gulp-imagemin');
 
-var path = {
-  app: 'app/Resources/themes',
-  bower_components: './bower_components'
+/** Variable to edit **/
+var vars = {
+  theme: 'default',
+
+  //Include only the CSS from vendors or uncomment the needed lines
+  //Do not include minified files
+  lib_css: [
+    './web/lib/bootstrap/dist/css/bootstrap.css',
+    './web/lib/font-awesome/css/font-awesome.css',
+    // './web/lib/ionicons/css/ionicons.css',
+    // './web/lib/flexslider/flexslider.css',
+    // './web/lib/fancybox/source/jquery.fancybox.js',
+  ],
+
+  //Include only the JS from vendors or uncomment the needed lines
+  //Do not include minified files
+  lib_js: [
+    './web/lib/jquery/dist/jquery.js',
+    './web/lib/bootstrap/dist/js/bootstrap.js',
+    './web/lib/fastclick/lib/fastclick.js'
+    // './web/lib/wow/dist/wow.js',
+    // './web/lib/jquery.scrollTo/jquery.scrollTo.js',
+    // './web/lib/flexslider/jquery.flexslider.js',
+    // './web/lib/backstretch/jquery.backstretch.js',
+    // './web/lib/fancybox/source/jquery.fancybox.css',
+  ]
 };
 
-gulp.task('sass', function() {
-  return sass(path.app + '/scss', { compass: true, style: 'compressed', sourcemap: true })
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-    })
-    .pipe(minifyCss({keepSpecialComments:0}))
-    .pipe(sourcemaps.write())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('web/css/'));
-});
+/** Please do not edit this vars **/
+var path = {
+  theme_dir: 'app/Resources/themes/'+vars.theme
+}
 
-gulp.task('imagemin', function() {
-    return gulp.src('web/img/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}]
-        }))
-        .pipe(gulp.dest('web/img'));
+gulp.task('app_less', function() {
+  gulp.src(path.theme_dir + '/less/front.less')
+      .pipe(sourcemaps.init())
+      .pipe(less())
+      .pipe(minifyCss({keepSpecialComments:0}))
+      .pipe(sourcemaps.write())
+      .pipe(rename({basename: 'app', suffix: '.min'}))
+      .pipe(sourcemaps.write('maps', {addComment: false}))
+      .pipe(gulp.dest('web/css/'));
 });
-
 
 /**
 * gulp-concat && gulp-uglify
@@ -46,15 +60,15 @@ gulp.task('imagemin', function() {
 *
 * Compile and minify js vendor (bower_components).
 */
-gulp.task('vendor', function() {
-    gulp.src([
-        './web/lib/chuck/dist/chuck.min.js'
-    ])
-    .pipe(concat('vendor.js'))
-    .pipe(uglify({mangle: true}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('web/js/'));
+gulp.task('vendor_css', function() {
+  gulp.src(vars.lib_css)
+  .pipe(sourcemaps.init())
+  .pipe(concat('vendor.css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(sourcemaps.write('maps', {addComment: false}))
+  .pipe(gulp.dest('web/css/'));
 });
+
 
 /**
 * gulp-concat && gulp-uglify
@@ -63,20 +77,48 @@ gulp.task('vendor', function() {
 *
 * Compile and minify js App (app/Resources/js).
 */
-gulp.task('app', function() {
-    gulp.src([
-        path.app + '/**/*.js'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
-    .pipe(uglify({mangle: true}).on('error', gutil.log))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('web/js/'));
+gulp.task('app_js', function() {
+  gulp.src(path.theme_dir + '/js/**/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(concat('app.js'))
+  .pipe(uglify({mangle: true}).on('error', gutil.log))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(sourcemaps.write('maps'))
+  .pipe(gulp.dest('web/js/'));
 });
 
+/**
+* gulp-concat && gulp-uglify
+* @see https://www.npmjs.com/package/gulp-concat
+* @see https://www.npmjs.com/package/gulp-uglify
+*
+* Compile and minify js vendor (bower_components).
+*/
+gulp.task('vendor_js', function() {
+  gulp.src(vars.lib_js)
+  .pipe(sourcemaps.init())
+  .pipe(concat('vendor.js'))
+  .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(sourcemaps.write('maps'))
+  .pipe(gulp.dest('web/js/'));
+});
+
+
+gulp.task('imagemin', function() {
+  return gulp.src('web/img/*')
+      .pipe(imagemin({
+          progressive: true,
+          svgoPlugins: [{removeViewBox: false}]
+      }))
+      .pipe(gulp.dest('web/img'));
+});
+
+
 gulp.task('default', [
-'vendor',
-'app',
-'sass'
+  'vendor_css',
+  'vendor_js',
+  'app_js',
+  'app_less',
+  'imagemin'
 ]);
