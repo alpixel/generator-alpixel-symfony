53b7d428cea9009b3f3aa053adb1ce0240aafc51
Benjamin Hubert <benjamin@alpixel.fr>
2016-03-17 16:46:40 +0100

    Update IntegrationController.php

--- app/templates/src/AppBundle/Controller/IntegrationController.php
+++ app/templates/src/AppBundle/Controller/IntegrationController.php
@@ -8,14 +8,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class IntegrationController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/ma-super-page", name="super_page")
      */
     public function defaultAction()
     {
-        return $this->render('page/homepage.html.twig', array(
+        return $this->render('page/super_page.html.twig', array(
 
         ));
     }
+    
 
 }
 
