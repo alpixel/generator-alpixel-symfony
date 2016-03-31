a7da62555afd355f406d9d0b0846c3e575cb9581
Benjamin Hubert <benjamin@alpixel.fr>
2016-03-31 11:13:28 +0200

    Update IntegrationController.php

--- app/templates/src/AppBundle/Controller/IntegrationController.php
+++ app/templates/src/AppBundle/Controller/IntegrationController.php
@@ -5,6 +5,13 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Ce controller est utilisé uniquement pour l'intégration des nouvelles pages sur lesquelles les développeurs ne sont
+ * pas encore intervenues.
+ * @developers : Quand vous dynamisez une intégration, merci de faire le ménage dans le fichier actuel. S'il est vide,
+ * merci de supprimer le fichier
+ * @integrators : Veillez à respecter les conventions de nommages des fichiers twig svp
+ */
 class IntegrationController extends Controller
 {
     /**
@@ -12,9 +19,9 @@ class IntegrationController extends Controller
      */
     public function defaultAction()
     {
-        return $this->render('page/super_page.html.twig', array(
+        return $this->render('page/super_page.html.twig', [
 
-        ));
+        ]);
     }
     
 
