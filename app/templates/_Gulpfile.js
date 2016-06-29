1fa007ee7565b9bfe97a8d9fa36dde62ac989b26
mickael <mickael@alpixel.fr>
2016-06-29 08:43:33 +0200

    MAJ generator files

--- app/templates/_Gulpfile.js
+++ app/templates/_Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var shell = require('gulp-shell');
+var rev = require('gulp-rev');
 var sourcemaps = require('gulp-sourcemaps');
 var cssnano = require('gulp-cssnano');
 var watch = require('gulp-watch');
@@ -14,51 +16,55 @@ var autoprefixer = require('gulp-autoprefixer');
 
 /** Variable to edit **/
 var vars = {
-  theme: 'default',
-  admin: 'admin',
+    theme: 'default',
+    admin: 'admin',
 
-  //Include only the CSS from vendors or uncomment the needed lines
-  //Do not include minified files
-  lib_css: [
-    './web/lib/font-awesome/css/font-awesome.css',
-    './web/lib/ionicons/css/ionicons.css',
-    // './web/lib/bootstrap/dist/css/bootstrap.css',
-    // './web/lib/flexslider/flexslider.css',
-    // './web/lib/fancybox/source/jquery.fancybox.js',
-  ],
+    //Include only the CSS from vendors or uncomment the needed lines
+    //Do not include minified files
+    lib_css: [
+        './web/lib/font-awesome/css/font-awesome.css',
+        './web/lib/ionicons/css/ionicons.css',
+        // './web/lib/bootstrap/dist/css/bootstrap.css',
+    ],
 
-  //Include only the JS from vendors or uncomment the needed lines
-  //Do not include minified files
-  lib_js: [
-    './web/lib/jquery/dist/jquery.js',
-    './web/lib/bootstrap/dist/js/bootstrap.js',
-    './web/lib/fastclick/lib/fastclick.js',
-    // './web/lib/wow/dist/wow.js',
-    // './web/lib/jquery.scrollTo/jquery.scrollTo.js',
-    // './web/lib/flexslider/jquery.flexslider.js',
-    // './web/lib/backstretch/jquery.backstretch.js',
-    // './web/lib/fancybox/source/jquery.fancybox.js',
-  ]
+    //Include only the JS from vendors or uncomment the needed lines
+    //Do not include minified files
+    lib_js: [
+        './web/lib/jquery/dist/jquery.js',
+        './web/lib/bootstrap/dist/js/bootstrap.js',
+        './web/lib/fastclick/lib/fastclick.js',
+        './web/lib/wow/dist/wow.js',
+        './web/lib/jquery.scrollTo/jquery.scrollTo.js',
+        './web/lib/flexslider/jquery.flexslider.js',
+        './web/lib/backstretch/jquery.backstretch.js',
+        './web/lib/fancybox/source/jquery.fancybox.js',
+    ]
 };
 
 /** Please do not edit this vars **/
 var path = {
-  theme_dir: 'app/Resources/themes/'+vars.theme,
-  admin_dir: 'app/Resources/themes/' + vars.admin
+    theme_dir: 'app/Resources/themes/'+vars.theme,
+    admin_dir: 'app/Resources/themes/' + vars.admin
 }
 
 gulp.task('app_less', function() {
-  gulp.src(path.theme_dir + '/less/front.less')
+    gulp.src(path.theme_dir + '/less/front.less')
       .pipe(sourcemaps.init())
       .pipe(less())
       .pipe(cssnano({
         'postcss-minify-font-values': true
       }))
-      .pipe(autoprefixer("last 2 versions"))
-      .pipe(sourcemaps.write())
-      .pipe(rename({basename: 'app', suffix: '.min'}))
-      .pipe(sourcemaps.write('maps', {addComment: false}))
-      .pipe(gulp.dest('web/css/'));
+      .pipe(autoprefixer({
+            browsers:"> 1%, last 2 versions, Safari >= 8"
+        }))
+      .pipe(rev())
+        .pipe(sourcemaps.write('css/maps', {addComment: false}))
+        .pipe(gulp.dest('web'))
+        .pipe(rev.manifest('app/Resources/assets/rev-manifest.json', {
+            merge: true,
+            base: process.cwd() + '/app/Resources/assets'
+        }))
+        .pipe(gulp.dest('app/Resources/assets'));
 });
 
 /**
@@ -68,18 +74,25 @@ gulp.task('app_less', function() {
 *
 * Compile and minify js vendor (bower_components).
 */
-gulp.task('vendor_css', function() {
-  gulp.src(vars.lib_css)
-  .pipe(sourcemaps.init())
-  .pipe(rewriteCSS({destination: './web/css'}))
-  .pipe(concat('vendor.css'))
-  .pipe(cssnano({
-    'postcss-minify-font-values': true
-  }))
-  .pipe(rename({suffix: '.min'}))
-  .pipe(autoprefixer("last 2 versions"))
-  .pipe(sourcemaps.write('maps', {addComment: false}))
-  .pipe(gulp.dest('web/css/'));
+gulp.task('vendor_css', function () {
+    gulp.src(vars.lib_css)
+        .pipe(sourcemaps.init())
+        .pipe(rewriteCSS({destination: './web/css'}))
+        .pipe(concat('css/vendor.min.css'))
+        .pipe(cssnano({
+            'postcss-minify-font-values': true
+        }))
+        .pipe(autoprefixer({
+            browsers:"> 1%, last 2 versions, Safari >= 8"
+        }))
+        .pipe(rev())
+        .pipe(sourcemaps.write('css/maps', {addComment: false}))
+        .pipe(gulp.dest('web'))
+        .pipe(rev.manifest('app/Resources/assets/rev-manifest.json', {
+            merge: true,
+            base: process.cwd() + '/app/Resources/assets'
+        }))
+        .pipe(gulp.dest('app/Resources/assets'));
 });
 
 
@@ -91,13 +104,18 @@ gulp.task('vendor_css', function() {
 * Compile and minify js App (app/Resources/js).
 */
 gulp.task('app_js', function() {
-  gulp.src(path.theme_dir + '/js/**/*.js')
-  .pipe(sourcemaps.init())
-  .pipe(concat('app.js'))
-  .pipe(uglify({mangle: true}).on('error', gutil.log))
-  .pipe(rename({suffix: '.min'}))
-  .pipe(sourcemaps.write('maps'))
-  .pipe(gulp.dest('web/js/'));
+    gulp.src(path.theme_dir + '/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('js/app.min.js'))
+        .pipe(uglify({mangle: true}).on('error', gutil.log))
+        .pipe(rev())
+        .pipe(sourcemaps.write('js/maps'))
+        .pipe(gulp.dest('web'))
+        .pipe(rev.manifest('app/Resources/assets/rev-manifest.json', {
+            merge: true,
+            base: process.cwd() + '/app/Resources/assets'
+        }))
+        .pipe(gulp.dest('app/Resources/assets'));
 });
 
 /**
@@ -108,13 +126,18 @@ gulp.task('app_js', function() {
 * Compile and minify js vendor (bower_components).
 */
 gulp.task('vendor_js', function() {
-  gulp.src(vars.lib_js)
-  .pipe(sourcemaps.init())
-  .pipe(concat('vendor.js'))
-  .pipe(uglify())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(sourcemaps.write('maps'))
-  .pipe(gulp.dest('web/js/'));
+    gulp.src(vars.lib_js)
+        .pipe(sourcemaps.init())
+        .pipe(concat('js/vendor.min.js'))
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(sourcemaps.write('js/maps'))
+        .pipe(gulp.dest('web'))
+        .pipe(rev.manifest('app/Resources/assets/rev-manifest.json', {
+            merge: true,
+            base: process.cwd() + '/app/Resources/assets'
+        }))
+        .pipe(gulp.dest('app/Resources/assets'));
 });
 
 
@@ -135,7 +158,9 @@ gulp.task('wysiwyg_less', function () {
         .pipe(cssnano({
             'postcss-minify-font-values': true
         }))
-        .pipe(autoprefixer("last 2 versions"))
+        .pipe(autoprefixer({
+            browsers:"> 1%, last 2 versions, Safari >= 8"
+        }))
         .pipe(sourcemaps.write('css/maps', {addComment: false}))
         .pipe(gulp.dest('web'));
 });
